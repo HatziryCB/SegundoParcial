1ea2231fcfb66f500d79ddf2399ddf39e2021a83
Hatziry Chacón <hchaconb@miumg.edu.gt>
2022-04-30 23:49:40 -0600

    Creacion de clase padre e hija, creacion de atributos de clase

---
+++ Ejercicio1/FigurasGeometricas.java
@@ -0,0 +1,9 @@
+package Ejercicio1;
+
+public class FigurasGeometricas {
+    protected double area;
+    
+    public FigurasGeometricas(double area) {
+        this.area = area;
+    }
+}
---
+++ Ejercicio1/Poligonos.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Poligonos extends FigurasGeometricas{
+    private double perimetro;
+    private double base;
+    private double altura;
+    private double lado;
+
+public Poligonos(double area, double perimetro) {
+    super(area);
+    this.perimetro=perimetro;
+}
+
+}
