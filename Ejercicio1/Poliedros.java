e76a235c172fee7303810610f697bc8c84546511
Hatziry Chacón <hchaconb@miumg.edu.gt>
2022-05-01 00:41:54 -0600

    Metodo para calcular area y volumen de un cubo

--- Ejercicio1/Poliedros.java
+++ Ejercicio1/Poliedros.java
@@ -1,10 +1,10 @@
 package Ejercicio1;
 
 public class Poliedros extends FigurasGeometricas{
-private double volumen; 
-private double radio;
-private double lado; 
-private double altura; 
+    private  double volumen; 
+    private  double radio;
+    private  double lado; 
+    private  double altura; 
 
  public Poliedros(double volumen, double radio, double lado, double altura, double area) {
         super(area);
@@ -13,5 +13,11 @@ private double altura;
         this.lado = lado;
         this.altura = altura;
     }
- 
+ public void cubo(double area, double lado, double volumen){
+area= 6*Math.pow(lado, 2);
+volumen=Math.pow(lado, 3);
+     System.out.println("El área del cubo es: "+area);
+     System.out.println("\nEl volumen del cubo es: "+volumen);
+}
+
 }
